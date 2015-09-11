899302f626eb42ead465633b89c5c4dcca3a48c7
limonadeht <limonadeht@yahoo.co.jp>
2015-09-11 19:19:38 +0900

    bug fix

--- src/lycheisuemon/multiengineering/block/BasicGenerator.java
+++ src/lycheisuemon/multiengineering/block/BasicGenerator.java
@@ -112,27 +112,25 @@ public class BasicGenerator extends BlockContainer
 		}
 	}
 
-	public void onBlockPlacedBy(World world, int x, int y, int z, EntityLivingBase entity, ItemStack itemstack) {
-		int direction = MathHelper.floor_double((double) (entity.rotationYaw * 4.0F / 360.0F) + 0.5D) & 3;
+	public void onBlockPlacedBy(World world, int x, int y, int z, EntityLivingBase entityplayer, ItemStack itemstack)
+	{
+		int l = MathHelper.floor_double((double)(entityplayer.rotationYaw * 4.0F / 360.0F) + 0.5D) & 3;
 
-		if (direction == 0) {
+		if(l == 0)
+		{
 			world.setBlockMetadataWithNotify(x, y, z, 2, 2);
 		}
-
-		if (direction == 1) {
+		if(l == 1)
+		{
 			world.setBlockMetadataWithNotify(x, y, z, 5, 2);
 		}
-
-		if (direction == 2) {
+		if(l == 2)
+		{
 			world.setBlockMetadataWithNotify(x, y, z, 3, 2);
 		}
-
-		if (direction == 3) {
+		if(l == 3)
+		{
 			world.setBlockMetadataWithNotify(x, y, z, 4, 2);
-		}
-
-		if (itemstack.hasDisplayName()) {
-			((TileEntityBasicGenerator) world.getTileEntity(x, y, z)).furnaceName(itemstack.getDisplayName());
 		}
 	}
 
--- src/lycheisuemon/multiengineering/gui/GuiBasicGenerator.java
+++ src/lycheisuemon/multiengineering/gui/GuiBasicGenerator.java
@@ -1,5 +1,7 @@
 package lycheisuemon.multiengineering.gui;
 
+import org.lwjgl.opengl.GL11;
+
 import lycheisuemon.multiengineering.tileentity.TileEntityBasicGenerator;
 import net.minecraft.client.gui.inventory.GuiContainer;
 import net.minecraft.client.resources.I18n;
@@ -31,8 +33,21 @@ public class GuiBasicGenerator extends GuiContainer
 	}
 
 	@Override
-	protected void drawGuiContainerBackgroundLayer(float var1, int var2, int var3)
-	{
+	protected void drawGuiContainerBackgroundLayer(float var1, int var2, int var3) {
+		 GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
+	        this.mc.getTextureManager().bindTexture(bground);
+	        int k = (this.width - this.xSize) / 2;
+	        int l = (this.height - this.ySize) / 2;
+	        this.drawTexturedModalRect(k, l, 0, 0, this.xSize, this.ySize);
+	        int i1;
 
+	        if (this.basicGenerator.isBurning())
+	        {
+	            i1 = this.basicGenerator.getBurnTimeRemainingScaled(12);
+	            this.drawTexturedModalRect(k + 56, l + 36 + 12 - i1, 176, 12 - i1, 14, i1 + 2);
+	        }
+
+	        i1 = this.basicGenerator.getCookProgressScaled(24);
+	        this.drawTexturedModalRect(k + 79, l + 34, 176, 14, i1 + 1, 16);
 	}
 }
